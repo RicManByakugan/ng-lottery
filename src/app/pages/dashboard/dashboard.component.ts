df5062429137358a476e10ee480596475ca7982b
Ratovonirina <eric.ratovonirina@gmail.com>
2024-12-01 15:53:15 +0330

    main : add navigation indication

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [RouterLink, CommonModule, RouterOutlet],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  currentRoute = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      const url = this.router.url;
+      this.currentRoute = this.getRouteName(url);
+    });
+  }
+
+  private getRouteName(url: string): string {
+    const map: { [key: string]: string } = {
+      '/dashboard/kingcoins': 'KingCoins',
+      '/dashboard/lottery': 'Loterie',
+      '/dashboard/draw': 'Tirage',
+      '/dashboard/winning': 'Resultat',
+    };
+    return map[url as keyof typeof map] || 'Unknown';
+  }
 }
--- src/app/pages/dashboard/winning/winning.component.ts
+++ src/app/pages/dashboard/winning/winning.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-winning',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './winning.component.html',
   styleUrl: './winning.component.scss'
 })
 export class WinningComponent {
+  navItem1: boolean = true
+  navItem2: boolean = false
 
+  toggleNavItem() {
+    this.navItem1 =!this.navItem1
+    this.navItem2 =!this.navItem2
+  }
 }
